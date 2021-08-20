dcf946633e9d5a8bb80d56a454323fabcbd8bc7e
Sayak Paul <spsayakpaul@gmail.com>
2021-08-20 14:29:12 +0530

    renaming

--- build/compile_pipeline.py
+++ build/compile_pipeline.py
@@ -1,67 +1,76 @@
 
+import os
 import tfx
+import logging
+import argparse
 
-def create_pipeline(pipeline_name: str, pipeline_root: str, data_root: str,
-                     module_file: str, serving_model_dir: str, project_id: str,
-                     region: str, use_gpu: bool) -> tfx.dsl.Pipeline:
-  """Implements the penguin pipeline with TFX."""
-  # Brings data into the pipeline or otherwise joins/converts training data.
-  example_gen = tfx.components.CsvExampleGen(input_base=data_root)
 
-  # NEW: Configuration for Vertex AI Training.
-  # This dictionary will be passed as `CustomJobSpec`.
-  vertex_job_spec = {
-      'project': project_id,
-      'worker_pool_specs': [{
-          'machine_spec': {
-              'machine_type': 'n1-standard-4',
-          },
-          'replica_count': 1,
-          'container_spec': {
-              'image_uri': 'gcr.io/tfx-oss-public/tfx:{}'.format(tfx.__version__),
-          },
-      }],
-  }
-  if use_gpu:
-    # See https://cloud.google.com/vertex-ai/docs/reference/rest/v1/MachineSpec#acceleratortype
-    # for available machine types.
-    vertex_job_spec['worker_pool_specs'][0]['machine_spec'].update({
-        'accelerator_type': 'NVIDIA_TESLA_K80',
-        'accelerator_count': 1
-    })
+def get_args():
+    parser = argparse.ArgumentParser()
 
-  # Trains a model using Vertex AI Training.
-  # NEW: We need to specify a Trainer for GCP with related configs.
-  trainer = tfx.extensions.google_cloud_ai_platform.Trainer(
-      module_file=module_file,
-      examples=example_gen.outputs['examples'],
-      train_args=tfx.proto.TrainArgs(num_steps=100),
-      eval_args=tfx.proto.EvalArgs(num_steps=5),
-      custom_config={
-          tfx.extensions.google_cloud_ai_platform.ENABLE_UCAIP_KEY:
-              True,
-          tfx.extensions.google_cloud_ai_platform.UCAIP_REGION_KEY:
-              region,
-          tfx.extensions.google_cloud_ai_platform.TRAINING_ARGS_KEY:
-              vertex_job_spec,
-          'use_gpu':
-              use_gpu,
-      })
+    parser.add_argument(
+        '--project',  
+        type=str,
+    )
 
-  # Pushes the model to a filesystem destination.
-  pusher = tfx.components.Pusher(
-      model=trainer.outputs['model'],
-      push_destination=tfx.proto.PushDestination(
-          filesystem=tfx.proto.PushDestination.Filesystem(
-              base_directory=serving_model_dir)))
+    parser.add_argument(
+        '--region', 
+        type=str,
+    )
 
-  components = [
-      example_gen,
-      trainer,
-      pusher,
-  ]
-  
-  return tfx.dsl.Pipeline(
-      pipeline_name=pipeline_name,
-      pipeline_root=pipeline_root,
-      components=components)
+    parser.add_argument(
+        '--pipeline-name', 
+        type=str,
+    )
+
+    parser.add_argument(
+        '--pipeline-root', 
+        type=str,
+    )
+
+    parser.add_argument(
+        '--data-root', 
+        type=str,
+    )
+
+    parser.add_argument(
+        '--module-root', 
+        type=str,
+    )
+
+    parser.add_argument(
+        '--serving-model-dir', 
+        type=str,
+    )
+    
+    return parser.parse_args()
+
+
+def compile_pipeline(args):
+    PIPELINE_DEFINITION_FILE = PIPELINE_NAME + '_pipeline.json'
+
+    runner = tfx.orchestration.experimental.KubeflowV2DagRunner(
+        config=tfx.orchestration.experimental.KubeflowV2DagRunnerConfig(),
+        output_filename=args.pipeline_definition_file)
+    return runner.run(
+        create_pipeline(
+            pipeline_name=args.pipeline_name,
+            pipeline_root=args.pipeline_root,
+            data_root=args.data_root,
+            module_file=os.path.join(args.module_root + "penguin_trainer.py"),
+            serving_model_dir=args.serving_model_dir,
+            project_id=args.project,
+            region=args.region,
+            # We will use CPUs only for now.
+            use_gpu=False
+        ),
+        write_out=True
+    )
+
+def main():
+    args = get_args()
+    result = compile_pipeline(args)
+    logging.info(result)
+
+if __name__ == "__main__":
+    main()
