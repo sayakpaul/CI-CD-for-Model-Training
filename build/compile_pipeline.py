58c8cec6a9e9770e3702e943a089ae95c3f84047
Sayak Paul <spsayakpaul@gmail.com>
2021-08-20 15:12:13 +0530

    adding changes

--- build/compile_pipeline.py
+++ build/compile_pipeline.py
@@ -1,57 +1,49 @@
-
 import os
 import tfx
 import logging
 import argparse
+from . import create_pipeline
 
 
 def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--project',  
+        "--pipeline-name",
         type=str,
     )
 
     parser.add_argument(
-        '--region', 
+        "--pipeline-root",
         type=str,
     )
 
     parser.add_argument(
-        '--pipeline-name', 
+        "--data-root",
         type=str,
     )
 
     parser.add_argument(
-        '--pipeline-root', 
+        "--module-root",
         type=str,
     )
 
     parser.add_argument(
-        '--data-root', 
+        "--serving-model-dir",
         type=str,
     )
 
-    parser.add_argument(
-        '--module-root', 
-        type=str,
-    )
-
-    parser.add_argument(
-        '--serving-model-dir', 
-        type=str,
-    )
-    
     return parser.parse_args()
 
 
 def compile_pipeline(args):
-    PIPELINE_DEFINITION_FILE = PIPELINE_NAME + '_pipeline.json'
+    pipeline_definition_file = args.pipeline_name + ".json"
 
     runner = tfx.orchestration.experimental.KubeflowV2DagRunner(
         config=tfx.orchestration.experimental.KubeflowV2DagRunnerConfig(),
-        output_filename=args.pipeline_definition_file)
+        output_filename=pipeline_definition_file,
+    )
+
     return runner.run(
         create_pipeline(
             pipeline_name=args.pipeline_name,
@@ -59,18 +51,20 @@ def compile_pipeline(args):
             data_root=args.data_root,
             module_file=os.path.join(args.module_root + "penguin_trainer.py"),
             serving_model_dir=args.serving_model_dir,
-            project_id=args.project,
-            region=args.region,
+            project_id=os.getenv("PROJECT"),
+            region=os.getenv("REGION"),
             # We will use CPUs only for now.
-            use_gpu=False
+            use_gpu=False,
         ),
-        write_out=True
+        write_out=True,
     )
+
 
 def main():
     args = get_args()
     result = compile_pipeline(args)
     logging.info(result)
+
 
 if __name__ == "__main__":
     main()
--- build/penguin_trainer.py
+++ build/penguin_trainer.py
@@ -1,4 +1,3 @@
-
 # Copied from https://www.tensorflow.org/tfx/tutorials/tfx/penguin_simple and
 # slightly modified run_fn() to add distribution_strategy.
 
@@ -13,9 +12,12 @@ from tfx import v1 as tfx
 from tfx_bsl.public import tfxio
 
 _FEATURE_KEYS = [
-    'culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g'
+    "culmen_length_mm",
+    "culmen_depth_mm",
+    "flipper_length_mm",
+    "body_mass_g",
 ]
-_LABEL_KEY = 'species'
+_LABEL_KEY = "species"
 
 _TRAIN_BATCH_SIZE = 20
 _EVAL_BATCH_SIZE = 10
@@ -27,108 +29,109 @@ _FEATURE_SPEC = {
     **{
         feature: tf.io.FixedLenFeature(shape=[1], dtype=tf.float32)
         for feature in _FEATURE_KEYS
-    }, _LABEL_KEY: tf.io.FixedLenFeature(shape=[1], dtype=tf.int64)
+    },
+    _LABEL_KEY: tf.io.FixedLenFeature(shape=[1], dtype=tf.int64),
 }
 
 
-def _input_fn(file_pattern: List[str],
-              data_accessor: tfx.components.DataAccessor,
-              schema: schema_pb2.Schema,
-              batch_size: int) -> tf.data.Dataset:
-  """Generates features and label for training.
+def _input_fn(
+    file_pattern: List[str],
+    data_accessor: tfx.components.DataAccessor,
+    schema: schema_pb2.Schema,
+    batch_size: int,
+) -> tf.data.Dataset:
+    """Generates features and label for training.
 
-  Args:
-    file_pattern: List of paths or patterns of input tfrecord files.
-    data_accessor: DataAccessor for converting input to RecordBatch.
-    schema: schema of the input data.
-    batch_size: representing the number of consecutive elements of returned
-      dataset to combine in a single batch
+    Args:
+      file_pattern: List of paths or patterns of input tfrecord files.
+      data_accessor: DataAccessor for converting input to RecordBatch.
+      schema: schema of the input data.
+      batch_size: representing the number of consecutive elements of returned
+        dataset to combine in a single batch
 
-  Returns:
-    A dataset that contains (features, indices) tuple where features is a
-      dictionary of Tensors, and indices is a single Tensor of label indices.
-  """
-  return data_accessor.tf_dataset_factory(
-      file_pattern,
-      tfxio.TensorFlowDatasetOptions(
-          batch_size=batch_size, label_key=_LABEL_KEY),
-      schema=schema).repeat()
+    Returns:
+      A dataset that contains (features, indices) tuple where features is a
+        dictionary of Tensors, and indices is a single Tensor of label indices.
+    """
+    return data_accessor.tf_dataset_factory(
+        file_pattern,
+        tfxio.TensorFlowDatasetOptions(batch_size=batch_size, label_key=_LABEL_KEY),
+        schema=schema,
+    ).repeat()
 
 
 def _make_keras_model() -> tf.keras.Model:
-  """Creates a DNN Keras model for classifying penguin data.
+    """Creates a DNN Keras model for classifying penguin data.
 
-  Returns:
-    A Keras Model.
-  """
-  # The model below is built with Functional API, please refer to
-  # https://www.tensorflow.org/guide/keras/overview for all API options.
-  inputs = [keras.layers.Input(shape=(1,), name=f) for f in _FEATURE_KEYS]
-  d = keras.layers.concatenate(inputs)
-  for _ in range(2):
-    d = keras.layers.Dense(8, activation='relu')(d)
-  outputs = keras.layers.Dense(3)(d)
+    Returns:
+      A Keras Model.
+    """
+    # The model below is built with Functional API, please refer to
+    # https://www.tensorflow.org/guide/keras/overview for all API options.
+    inputs = [keras.layers.Input(shape=(1,), name=f) for f in _FEATURE_KEYS]
+    d = keras.layers.concatenate(inputs)
+    for _ in range(2):
+        d = keras.layers.Dense(8, activation="relu")(d)
+    outputs = keras.layers.Dense(3)(d)
 
-  model = keras.Model(inputs=inputs, outputs=outputs)
-  model.compile(
-      optimizer=keras.optimizers.Adam(1e-2),
-      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-      metrics=[keras.metrics.SparseCategoricalAccuracy()])
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(
+        optimizer=keras.optimizers.Adam(1e-2),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    )
 
-  model.summary(print_fn=logging.info)
-  return model
+    model.summary(print_fn=logging.info)
+    return model
 
 
 # NEW: Read `use_gpu` from the custom_config of the Trainer.
 #      if it uses GPU, enable MirroredStrategy.
 def _get_distribution_strategy(fn_args: tfx.components.FnArgs):
-  if fn_args.custom_config.get('use_gpu', False):
-    logging.info('Using MirroredStrategy with one GPU.')
-    return tf.distribute.MirroredStrategy(devices=['device:GPU:0'])
-  return None
+    if fn_args.custom_config.get("use_gpu", False):
+        logging.info("Using MirroredStrategy with one GPU.")
+        return tf.distribute.MirroredStrategy(devices=["device:GPU:0"])
+    return None
 
 
 # TFX Trainer will call this function.
 def run_fn(fn_args: tfx.components.FnArgs):
-  """Train the model based on given args.
+    """Train the model based on given args.
 
-  Args:
-    fn_args: Holds args used to train the model as name/value pairs.
-  """
+    Args:
+      fn_args: Holds args used to train the model as name/value pairs.
+    """
 
-  # This schema is usually either an output of SchemaGen or a manually-curated
-  # version provided by pipeline author. A schema can also derived from TFT
-  # graph if a Transform component is used. In the case when either is missing,
-  # `schema_from_feature_spec` could be used to generate schema from very simple
-  # feature_spec, but the schema returned would be very primitive.
-  schema = schema_utils.schema_from_feature_spec(_FEATURE_SPEC)
+    # This schema is usually either an output of SchemaGen or a manually-curated
+    # version provided by pipeline author. A schema can also derived from TFT
+    # graph if a Transform component is used. In the case when either is missing,
+    # `schema_from_feature_spec` could be used to generate schema from very simple
+    # feature_spec, but the schema returned would be very primitive.
+    schema = schema_utils.schema_from_feature_spec(_FEATURE_SPEC)
 
-  train_dataset = _input_fn(
-      fn_args.train_files,
-      fn_args.data_accessor,
-      schema,
-      batch_size=_TRAIN_BATCH_SIZE)
-  eval_dataset = _input_fn(
-      fn_args.eval_files,
-      fn_args.data_accessor,
-      schema,
-      batch_size=_EVAL_BATCH_SIZE)
+    train_dataset = _input_fn(
+        fn_args.train_files, fn_args.data_accessor, schema, batch_size=_TRAIN_BATCH_SIZE
+    )
+    eval_dataset = _input_fn(
+        fn_args.eval_files, fn_args.data_accessor, schema, batch_size=_EVAL_BATCH_SIZE
+    )
 
-  # NEW: If we have a distribution strategy, build a model in a strategy scope.
-  strategy = _get_distribution_strategy(fn_args)
-  if strategy is None:
-    model = _make_keras_model()
-  else:
-    with strategy.scope():
-      model = _make_keras_model()
+    # NEW: If we have a distribution strategy, build a model in a strategy scope.
+    strategy = _get_distribution_strategy(fn_args)
+    if strategy is None:
+        model = _make_keras_model()
+    else:
+        with strategy.scope():
+            model = _make_keras_model()
 
-  model.fit(
-      train_dataset,
-      steps_per_epoch=fn_args.train_steps,
-      validation_data=eval_dataset,
-      validation_steps=fn_args.eval_steps,
-      epochs=1)
+    model.fit(
+        train_dataset,
+        steps_per_epoch=fn_args.train_steps,
+        validation_data=eval_dataset,
+        validation_steps=fn_args.eval_steps,
+        epochs=1,
+    )
 
-  # The result of the training should be saved in `fn_args.serving_model_dir`
-  # directory.
-  model.save(fn_args.serving_model_dir, save_format='tf')
+    # The result of the training should be saved in `fn_args.serving_model_dir`
+    # directory.
+    model.save(fn_args.serving_model_dir, save_format="tf")
