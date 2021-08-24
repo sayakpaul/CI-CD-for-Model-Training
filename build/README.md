ad57882f9619b2b1f7b3e34b751e4e2e36aea5aa
Sayak Paul <spsayakpaul@gmail.com>
2021-08-24 10:46:08 +0530

    modifications to spec

--- build/README.md
+++ build/README.md
@@ -1,2 +1,2 @@
-The Python scripts have been taken from this tutorial
+The Python scripts have been taken and modified from this tutorial
 [Vertex AI Training with TFX and Vertex Pipelines](https://www.tensorflow.org/tfx/tutorials/tfx/gcp/vertex_pipelines_vertex_training).
--- utils/config.py
+++ utils/config.py
@@ -1,17 +1,17 @@
 import os
 
 # GCP
-GCP_PROJECT = os.getenv("_PROJECT")
-GCP_REGION = os.getenv("_REGION")
+GCP_PROJECT = os.getenv("PROJECT")
+GCP_REGION = os.getenv("REGION")
 
 # Data
-DATA_ROOT = os.getenv("_DATA_ROOT")
+DATA_ROOT = os.getenv("DATA_ROOT")
 
 # Training and serving
-MODULE_ROOT = os.getenv("_MODULE_ROOT")
+MODULE_ROOT = os.getenv("MODULE_ROOT")
 MODULE_FILE = os.path.join(MODULE_ROOT + "penguin_trainer.py")
-SERVING_MODEL_DIR = os.getenv("_MODULE_ROOT")
+SERVING_MODEL_DIR = os.getenv("MODULE_ROOT")
 
 # Pipeline
-PIPELINE_NAME = os.getenv("_PIPELINE_NAME")
-PIPELINE_ROOT = os.getenv("_SERVING_MODEL_DIR")
+PIPELINE_NAME = os.getenv("PIPELINE_NAME")
+PIPELINE_ROOT = os.getenv("SERVING_MODEL_DIR")
