e103d0ed2715bdab239e17d7d4d73cef92c71c98
Sayak Paul <spsayakpaul@gmail.com>
2021-08-24 19:20:34 +0530

    pesky bugs will kill you

--- utils/config.py
+++ utils/config.py
@@ -10,7 +10,7 @@ DATA_ROOT = os.getenv("DATA_ROOT")
 # Training and serving
 TFX_IMAGE_URI = os.getenv("TFX_IMAGE_URI")
 MODULE_ROOT = os.getenv("MODULE_ROOT")
-MODULE_FILE = os.path.join(MODULE_ROOT + "penguin_trainer.py")
+MODULE_FILE = os.path.join(MODULE_ROOT, "penguin_trainer.py")
 SERVING_MODEL_DIR = os.getenv("MODULE_ROOT")
 
 # Pipeline
